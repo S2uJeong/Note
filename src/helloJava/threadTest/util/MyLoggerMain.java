149cc37fe5bc3be451bd4ef41a73f5813f4174e9
sujeong <sj980204@gmail.com>
2024-10-02 04:22:36 +0900

    [code] Thread 실습 Utils

---
+++ src/helloJava/threadTest/util/MyLoggerMain.java
@@ -0,0 +1,10 @@
+package helloJava.threadTest.util;
+
+import static helloJava.threadTest.util.MyLogger.*;
+
+public class MyLoggerMain {
+    public static void main(String[] args) {
+        log("hello thread");
+        log(123);
+    }
+}
